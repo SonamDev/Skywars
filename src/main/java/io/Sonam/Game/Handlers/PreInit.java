f2c6504fc5728a99658628112b7cf89ee85d91fd
Sonam <cp.cp940@gmail.com>
2016-07-01 14:38:48 -0600

    nyanyanyanyanyanya

--- src/main/java/io/Sonam/Game/Handlers/PreInit.java
+++ src/main/java/io/Sonam/Game/Handlers/PreInit.java
@@ -3,9 +3,12 @@ package io.Sonam.Game.Handlers;
 import io.Sonam.Game.Menu.ItemStacks.MainItems;
 import io.Sonam.Game.SkyWars;
 import io.Sonam.Game.Utils.GameState;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,7 +26,8 @@ public class PreInit implements Listener {
     }
 
     @EventHandler
-    public void onJoinTest(PlayerJoinEvent e) {
+    public void onJoin(PlayerJoinEvent e) {
+        CraftPlayer craftPlayer = (CraftPlayer) e.getPlayer();
         e.getPlayer().setHealth(20.0);
         e.getPlayer().setFoodLevel(20);
         e.getPlayer().teleport(new Location(Bukkit.getWorld("Game"), 8.5, 12, 32.5, 180F, 0F));
@@ -32,6 +36,16 @@ public class PreInit implements Listener {
         e.getPlayer().getInventory().setItem(8, items.getLeaveGame());
         Bukkit.broadcastMessage(ChatColor.YELLOW + e.getPlayer().getName() + " joined!" + ChatColor.GREEN + " [" + Bukkit.getOnlinePlayers().size() + "/" + SkyWars.getGameManager().getMaxPlayers() + "]");
         e.setJoinMessage(null);
+
+        PacketPlayOutTitle title = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a(
+                "{\'text\':\'Welcome To\', \'color\':\'aqua\'}"
+        ));
+        PacketPlayOutTitle subtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a(
+                "{\'text\':\'Skywars UHC Mode\', \'color\':\'yellow\'}"
+        ));
+        craftPlayer.getHandle().playerConnection.sendPacket(title);
+        craftPlayer.getHandle().playerConnection.sendPacket(subtitle);
+
     }
 
     @EventHandler
