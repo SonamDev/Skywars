413c92a7e0440f899a1a3990915b03629aad52fb
Sonam <cp.cp940@gmail.com>
2016-07-01 11:30:47 -0600

    meow

---
+++ src/main/java/io/Sonam/Game/SkyWars.java
@@ -0,0 +1,15 @@
+package io.Sonam.Game;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkyWars extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
