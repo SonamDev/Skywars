d1b40addd1c4281b71c5e401bf30842b48b34943
Sonam <cp.cp940@gmail.com>
2016-07-04 17:09:10 -0600

    woops lol

--- src/main/java/io/Sonam/Game/Handlers/PreInit.java
+++ src/main/java/io/Sonam/Game/Handlers/PreInit.java
@@ -4,6 +4,7 @@ import io.Sonam.Game.Menu.ItemStacks.KitSelectorItems;
 import io.Sonam.Game.Menu.ItemStacks.MainItems;
 import io.Sonam.Game.SkyWars;
 import io.Sonam.Game.Utils.GameState;
+import io.Sonam.Game.Utils.Kits;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
 import org.bukkit.Bukkit;
@@ -39,7 +40,7 @@ public class PreInit implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         SkyWars.getPlayers().add(e.getPlayer().getUniqueId());
-        SkyWars.getKitSelected().put(e.getPlayer().getUniqueId(), "Default");
+        SkyWars.getKitSelected().put(e.getPlayer().getUniqueId(), Kits.DEFAULT);
         KitSelectorItems.clearAll(e.getPlayer());
         e.getPlayer().teleport(new Location(Bukkit.getWorld("2k"), 397.5, 8.0, -349.5, 0F, 0F));
         e.getPlayer().setHealth(20.0);
