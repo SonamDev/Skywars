500adde1805dd30f6bda215816a85da5415efd38
Sonam <cp.cp940@gmail.com>
2016-07-04 22:44:06 -0600

    oijadsfponiadsfponasdfoinpadsfdsfd

--- src/main/java/io/Sonam/Game/Handlers/ChestFiller.java
+++ src/main/java/io/Sonam/Game/Handlers/ChestFiller.java
@@ -33,14 +33,23 @@ public class ChestFiller {
             Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Randomizing...");
             ItemStack[] items = new ItemStack[27];
             for(int i = 0; i < items.length; i++) {
-                switch (random.nextInt(30)) {
+                switch (random.nextInt(100)) {
                     case 1:
+                        items[i] = new ItemStack(Material.LOG, 32);
+                        continue;
+                    case 8:
                         items[i] = new ItemStack(Material.LOG, 32);
                         continue;
                     case 2:
                         items[i] = new ItemStack(Material.STONE, 64);
                         continue;
+                    case 9:
+                        items[i] = new ItemStack(Material.STONE, 64);
+                        continue;
                     case 3:
+                        items[i] = new ItemStack(Material.STONE_SWORD);
+                        continue;
+                    case 10:
                         items[i] = new ItemStack(Material.STONE_SWORD);
                         continue;
                     case 4:
