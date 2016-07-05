0ad61e8e139829a8f21a7e6b3f6723d5f6c55996
Sonam <cp.cp940@gmail.com>
2016-07-04 23:09:52 -0600

    lihbadsfoihbafdsohibsdaf

--- src/main/java/io/Sonam/Game/Commands/Restart.java
+++ src/main/java/io/Sonam/Game/Commands/Restart.java
@@ -1,34 +1,15 @@
 package io.Sonam.Game.Commands;
 
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
 import io.Sonam.Game.SkyWars;
-import io.Sonam.Game.Utils.Utils;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class Restart implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
 
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF("Connect");
-        out.writeUTF("dev1a");
-
-        for(Player player : Bukkit.getOnlinePlayers()) {
-            player.sendPluginMessage(SkyWars.getPlugin(), "BungeeCord", out.toByteArray());
-        }
-
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Bukkit.shutdown();
+        SkyWars.getGameManager().endGame();
 
         return false;
     }
